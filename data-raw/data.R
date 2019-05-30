1d7f1291e994ba35eb5eea9c3acf6e9fce7c8fcc
Fiorella <fioreveg@gmail.com>
2019-05-30 12:44:41 -0500

    probe

---
+++ data-raw/data.R
@@ -0,0 +1,4 @@
+# setwd("D:/FIORELLA/DHI_2019/GITHUB/SPIF2/data-raw")
+# load("data.RData")
+#devtools::use_data(data)
+usethis::use_data("data")
--- data-raw/save_data.R
+++
@@ -1,4 +0,0 @@
-setwd("D:/FIORELLA/DHI_2019/GITHUB/SPIF2/data-raw")
-load("data.RData")
-devtools::use_data(data)
-#usethis::use_data("data")
